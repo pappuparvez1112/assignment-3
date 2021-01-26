8f89cb3fc016cf020ceab8e5c6c97c963c9430c3
pappu parvez <pappuparvezshazz@gmail.com>
2021-01-27 01:58:43 +0600

    update

--- assignment.js
+++ assignment.js
@@ -1,48 +1,49 @@
-//  function kilometerToMeter(kilometer){
-//     var meter=kilometer*1000;
-//     return meter;
-// }
-// var result= kilometerToMeter(50);
-// console.log(result);
+  
+  
+  function kilometerToMeter(kilometer){
+     var meter=kilometer*1000;
+     return meter;
+ }
+ var result= kilometerToMeter(50);
+ console.log(result);
 
 
 
 
-//  function budgetCalculator(watch,phone,laptop){
-//      var sum=watch+phone+laptop;
+  function budgetCalculator(watch,phone,laptop){
+      var sum=watch+phone+laptop;
     
-//      return sum;
-//  }
-//  var result=budgetCalculator(50,100,500);
-//  console.log(result);
+      return sum;
+  }
+  var result=budgetCalculator(50,100,500);
+  console.log(result);
 
 
 
 
-// function hotelCost(day){
-//     var rent=0;
-//     if(day<=10){
-//         rent=day*100;
-//     } else if(day<=20){
-//         var first=10*100;
-//         var left=day-10;
-//         var second=left*80;
-//         rent=first+second;
-//     } else{
-//         var first=10*100;
-//         var second=10*80;
-//         var left=day-20;
-//         var third=left*50;
-//         rent=first+second+third;
+ function hotelCost(day){
+     var rent=0;
+     if(day<=10){
+         rent=day*100;
+     } else if(day<=20){
+         var first=10*100;
+         var left=day-10;
+         var second=left*80;
+         rent=first+second;
+     } else{
+         var first=10*100;
+         var second=10*80;
+         var left=day-20;
+         var third=left*50;
+         rent=first+second+third;
 
-//     }
-//     return rent;
+     }
+     return rent;
 
-// }
-// var totalRent=hotelCost(13);
-// console.log(totalRent);
-
-
+ }
+ var totalRent=hotelCost(13);
+ console.log(totalRent);
 
 
-function megaFriend()
+
+
